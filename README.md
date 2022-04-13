5cbc1765e386f2c8e8b40ba58ca3ffd4f949c78a
Mishkin_Evgeny <mjos1995@gmail.com>
2022-04-13 09:44:56 +0400

    Дополнено readme

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Volgar pollution Adapter
-Адаптер для http://pogoda-sv.ru/->prometheus
+Набор приложений, позволяющий получать, хранить и визуализировать исторические данные для http://pogoda-sv.ru/pollcenter/volgar/ , который отдаёт только текущие значения загрязнений воздуха в жилом микрорайне "волгарь", г. Самара.
+
+![Preview](docs/volgar_pollution_preview.jpg)
+
+
+### Используемые стек:
+- FastAPI
+- Prometheus
+- Grafana
+
 ### Сборка и запуск в докере (Python):
 ```
 docker-compose up
 ```
-Используются файлы docker-compose.yml и Dockerfile из корня проекта.
+запускает три компонента:
+- adapter (трансляция данных с сервера pogodasv)
+- prometheus_volgar (хранения данных в БД)
+- grafana_volgar (визуализация данных)
+
+###
+
+Используются папка grafana, файлы docker-compose.yml, .env и Dockerfile из корня проекта. В переменных .env должно содержаться только MY_SECRET_PASS для пароля в админку графаны
--- api/router.py
+++ api/router.py
@@ -10,14 +10,9 @@ router = APIRouter()
 
 CONTENT_TYPE_LATEST = 'text/plain; version=0.0.4; charset=utf-8'
 
+
 @router.get('/pollutions')
 def get_pollutions():
-    # l1 = "# HELP PARAM1 Description\n"
-    # l2 = "# TYPE PARAM1 gauge\n"
-    # l3 = f"PARAM1 {a.gauge1}\n"
-    # result = ''.join((l1, l2, l3)).replace('\n', r'\n').encode('utf-8')
-    # headers = {'Content-Type': CONTENT_TYPE_LATEST}
-    # return Response(content=result, headers=headers)
     imp_dict = pogoda_handler.get_impurities_data()
     for key, impurity in imp_dict.items():
         metrics.meters.g1().labels(key, 'calc').set(float(impurity['value']))
@@ -25,11 +20,6 @@ def get_pollutions():
 
     return Response(content=metrics.generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
-# @router.get('/set_gauge1', status_code=200)
-# def set_gauge1(number:int):
-#     metrics.meters.c1().set(number)
-#     # metrics.meters.c1.set(number)
-#     return {'success': True}
 
 @router.get('/version', status_code=200)
 def get_version():
